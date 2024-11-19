2ed7b568f864f1fc309c7df2f6191cb576c09f90
garu-farcis <priyankagaru2024@gmail.com>
2024-11-19 18:30:43 +0100

    adding new files

--- conditional statement exercises/num_decreasing_order.py
+++ conditional statement exercises/num_decreasing_order.py
@@ -11,6 +11,9 @@
 # Given a number x, the task is to print the numbers from x to 0 in decreasing order in a single line.
 def decreaseorder():
     num = int(input("enter any number greater than 0"))
-    while i>0:
-        print(i)
+    while num!=0:
+        print(num)
+        num= num -1
+
+decreaseorder()
 
